6e6c4f022efbe4e8f9b9c85f9adac0531e675ff0
Joel Dankert <106259863+JoelDankert@users.noreply.github.com>
2023-04-21 18:38:46 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -10,27 +10,37 @@ def downloadbirdsound(soundid,name):
     f = open(desiredfolder+name+'.wav','wb')
     f.write(req.content)
     f.close()
+    req.close()
 
 def downloadmultipleids(ids,name):
-    if not os.path.exists(desiredfolder+name):
-        os.mkdir(desiredfolder+name)
+    #if not os.path.exists(desiredfolder+name):
+    #    os.mkdir(desiredfolder+name)
         
     for i,idc in enumerate(ids):
         downloadbirdsound(idc,f'{name}_{i+1}')
 
+#def getratings(assetid):
+#    checker = requests.get(f'https://macaulaylibrary.org/asset/{name}')
+#    checkertext = checker.text
+#    checkertext = checkertext[checkertext.find('RatingStars-count\">'):]
+#    ratings = checkertext[:checkcertext.find(' ratings')]
+#    return int(ratings)
 
-def getsoundfrombird(birdid,count,tag = 'song'):
-    link = f'https://media.ebird.org/catalog?taxonCode={birdid}&mediaType=audio&tag={tag}&sort=rating_rank_desc&regionCode=DE'
+def getsoundfrombird(birdid,count):
+    link = f'https://media.ebird.org/catalog?taxonCode={birdid}&mediaType=audio&sort=rating_rank_desc&regionCode=DE'
     req = requests.get(link)
     txt=req.text
-
+    req.close()
     soundids = []
 
     while len(soundids) < count:
         try:
             txt=txt[txt.find('assetId')+8:]
+            name = txt[:txt.find(',')]
             
-            soundids.append(txt[:txt.find(',')])
+            #rat = getratings(name)
+            #if (rat > 10):
+            soundids.append(name)
             
         except:
             print(f'ERROR: failed to collect sound: {birdid}: {len(soundbirds)}')
@@ -41,8 +51,9 @@ f = open('species.txt','r')
 txt=f.read()
 f.close()
 
-soundstodownload=[x.split(',') for x in txt.split('\n')]
+soundstodownload=[x.split(';') for x in txt.split('\n')]
 
 
 for x in soundstodownload:
-    downloadmultipleids(getsoundfrombird(x[1],int(x[2]),x[3]),x[0])
+    downloadmultipleids(getsoundfrombird(x[1],3),x[0])
+
