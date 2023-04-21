5ee15f815219d97af484903227ed46ee8e3a1b45
Joel Dankert <106259863+JoelDankert@users.noreply.github.com>
2023-04-21 14:15:35 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,2 +1,22 @@
+import webbrowser
+import time
+import os
 import requests
 
+downloadfolder = 'D:\\Downloads\\'
+desiredfolder = 'D:\\ebirdsounds\\'
+
+
+def downloadbirdsound(soundid,name):
+    webbrowser.open(f'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/{soundid}/audio')
+    time.sleep(3)
+    os.rename(downloadfolder+"audio",desiredfolder + name + '.wav')
+
+def downloadmultipleids(ids,names):
+    for n,idc in enumerate(ids):
+        downloadbirdsound(idc,names[idc])
+
+
+def getsoundfrombird(birdid, soundname)
+    link = 'https://media.ebird.org/catalog?taxonCode={birdid}&mediaType=audio&tag=song&sort=rating_rank_desc&regionCode=DE'
+    req = requests.get(link)
