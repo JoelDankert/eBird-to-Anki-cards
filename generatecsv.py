afec9bc32f93ffaa393170419b95aa5542645e49
Joel Dankert <106259863+JoelDankert@users.noreply.github.com>
2023-04-21 16:51:22 +0200

    Create generatecsv.py

---
+++ generatecsv.py
@@ -0,0 +1,19 @@
+import glob
+
+desiredfolder = 'C:\\Users\\joeld\\Desktop\\Python\\ebird\\sounds\\'
+
+files = glob.glob(f"{desiredfolder}*.wav")
+files = [x[len(desiredfolder):] for x in files]
+
+names = [x.replace('_','(')[:-4]+')' for x in files]
+
+filesanki = [f'[sounds:{x}]' for x in files]
+out = [[x,filesanki[i]] for i,x in enumerate(names)]
+out2 = [';'.join(x) for x in out]
+out3 = '\n'.join(out2)
+
+
+print(out3)
+
+with open("out.csv",'w') as f:
+    f.write(out3)
