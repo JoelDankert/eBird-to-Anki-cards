fbbee6997514af96f33678bff09e033d4040d3a3
Joel Dankert <106259863+JoelDankert@users.noreply.github.com>
2023-04-21 14:55:17 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,22 +1,38 @@
-import webbrowser
 import time
 import os
 import requests
 
-downloadfolder = 'D:\\Downloads\\'
 desiredfolder = 'D:\\ebirdsounds\\'
 
 
 def downloadbirdsound(soundid,name):
-    webbrowser.open(f'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/{soundid}/audio')
-    time.sleep(3)
-    os.rename(downloadfolder+"audio",desiredfolder + name + '.wav')
-
-def downloadmultipleids(ids,names):
-    for n,idc in enumerate(ids):
-        downloadbirdsound(idc,names[idc])
-
-
-def getsoundfrombird(birdid, soundname)
-    link = 'https://media.ebird.org/catalog?taxonCode={birdid}&mediaType=audio&tag=song&sort=rating_rank_desc&regionCode=DE'
+    link = f'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/{soundid}/audio'
     req = requests.get(link)
+    f = open(desiredfolder+name+'.wav','wb')
+    f.write(req.content)
+    f.close()
+
+def downloadmultipleids(ids,name):
+    for i,idc in enumerate(ids):
+        downloadbirdsound(idc,f'{name}\\{name}_{i}')
+
+
+def getsoundfrombird(birdid,count):
+    link = f'https://media.ebird.org/catalog?taxonCode={birdid}&mediaType=audio&tag=song&sort=rating_rank_desc&regionCode=DE'
+    req = requests.get(link)
+    txt=req.text
+
+    soundids = []
+
+    while len(soundids) < count:
+        try:
+            txt=txt[txt.find('assetId')+8:]
+            
+            soundids.append(txt[:txt.find(',')])
+            
+        except:
+            print(f'ERROR: failed to collect sound: {birdid}: {len(soundbirds)}')
+    
+    return soundids
+
+downloadmultipleids(getsoundfrombird('comchi1',5),'zilpzalp')
