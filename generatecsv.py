1112cf8bfd9de2b8085d08cf4dc77f191c68e620
Joel Dankert <106259863+JoelDankert@users.noreply.github.com>
2023-04-25 18:49:26 +0200

    Update generatecsv.py

--- generatecsv.py
+++ generatecsv.py
@@ -1,14 +1,14 @@
 import glob
 
-desiredfolder = 'D:\\ebirdsounds\\sounds\\raw\\'
+#desiredfolder = 'D:\\ebirdsounds\\sounds\\raw\\'
 desiredfolder = 'C:\\Users\\joeld\\Desktop\\Python\\ebird\\sounds\\'
 
-files = glob.glob(f"{desiredfolder}*.wav")
+files = glob.glob(f"{desiredfolder}*.mp3")
 files = [x[len(desiredfolder):] for x in files]
 
 names = [x.replace('_','(')[:-4]+')' for x in files]
 
-filesanki = [f'[sounds:{x}]' for x in files]
+filesanki = [f'[sound:{x}]' for x in files]
 out = [[x,filesanki[i]] for i,x in enumerate(names)]
 out2 = [';'.join(x) for x in out]
 out3 = '\n'.join(out2)
@@ -16,5 +16,5 @@ out3 = '\n'.join(out2)
 
 print(out3)
 
-with open("out.csv",'w') as f:
+with open('out.csv','w') as f:
     f.write(out3)
